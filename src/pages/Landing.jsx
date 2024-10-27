8222b5504ddb6d35cba637b9689a829fdd3afdf1
Saurav <saurav.jaswl@gmail.com>
2024-10-27 17:50:54 +0545

    removed comment

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,10 +11,6 @@ const searchCocktailsQuery = (searchTerm) => {
   return {
     queryKey: ["search", searchTerm || "all"],
     queryFn: async () => {
-      // UPDATE !!!
-      // Default to 'a' if no search term is provided since API has changed
-      // searchTerm = searchTerm || "a";
-
       const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
       return response.data.drinks;
     },
